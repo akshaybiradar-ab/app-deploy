84d06ff972fb10bb0c67ee7c64e80d93b2579701
Akshay Biradar <53450422+akshaybiradar-ab@users.noreply.github.com>
2025-08-06 14:32:14 +0530

    Update and rename .github/workflows/deploy-app.yaml to script.py

---
+++ script.py
@@ -0,0 +1,35 @@
+"""
+--------------------------------------------
+Python file with and without linting error
+--------------------------------------------
+# Linting Error
+def greet(name):
+    print(f"Hello, {name}!") # This line has an extra space that violates PEP8
+greet("John")
+
+# Workflow Error
+Run flake8 python/
+python/script.py:20:1:
+E302 expected 2 blank lines, found 1
+python/script.py:21:29:
+E261 at least two spaces before inline comment
+python/script.py:22:1:
+E305 expected 2 blank lines after class or function definition, found 0
+Error: Process completed with exit code 1.
+
+-------------------------------------------
+# No Linting Error
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+greet("John")
+------------------------------------------
+"""
+
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+greet("John")
